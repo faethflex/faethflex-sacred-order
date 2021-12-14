97e550cd5e47819de085556537311b86974bad12
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-13 22:44:04 -0500

    Added webpack bundling

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # faethflex-sacred-order
 faethflex-sacred-order
+
+- newly packaged for Docker
+
+## Setup
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
+
+``` bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
+```
--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-console.log(THREE);
+import './style.css'
+import * as THREE from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -10,7 +11,6 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-
 scene.add(mesh);
 
 // Sizes
