2d763657526249135f8fa3537265a0f277a67aff
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-13 21:32:52 -0500

    Added aspect ratio and camera

--- src/script.js
+++ src/script.js
@@ -9,3 +9,13 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
