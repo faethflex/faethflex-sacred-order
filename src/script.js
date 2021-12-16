b65b08ee8867037165dbf3027b0f9b775bcb285f
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-16 18:32:20 -0500

    Updated script ortho camera with aspect ratio

--- src/script.js
+++ src/script.js
@@ -21,7 +21,8 @@ const sizes = {
 
 // Camera
 // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100);
-const camera = new THREE.OrthographicCamera(- 1, 1, 1, - 1, 0.1, 100)
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100);
 camera.position.z = 3;
 scene.add(camera);
 
