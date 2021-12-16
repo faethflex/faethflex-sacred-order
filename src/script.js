9d1a3135b62c3cec64cadc784f0b68dc96120349
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-16 00:33:07 -0500

    Updated cube color

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: green });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
