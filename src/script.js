a0c48518ec006920800758e56c6553080e4ced8a
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-16 01:27:41 -0500

    Cleaned up build dependencies

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: green });
+const material = new THREE.MeshBasicMaterial({ color: 0x0066ff });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
