05c6343f204239a397c1f37bc8c859b2562d6f87
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-16 20:22:44 -0500

    Updated script to include new controls for camera

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -31,19 +32,9 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
-// Cursor
-const cursor = {
-    x: 0,
-    y: 0
-};
-
-window.addEventListener('mousemove', (event) =>
-{
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = - (event.clientY / sizes.height - 0.5);
-
-    console.log(cursor.x, cursor.y);
-});
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -60,11 +51,8 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime();
 
-    // Update camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-    camera.position.y = cursor.y * 3;
-    camera.lookAt(mesh.position);
+    // Update controls
+    controls.update();
 
     // Render
     renderer.render(scene, camera);
