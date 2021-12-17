7caf63de47936dca7e408a3e8ab7b0aead38fa67
Gregory Albert Fletcher Jr <faethflex@gmail.com>
2021-12-16 20:43:12 -0500

    Updated app for resizing and filling the display

--- src/script.js
+++ src/script.js
@@ -18,7 +18,21 @@ scene.add(mesh);
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-}
+};
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000);
